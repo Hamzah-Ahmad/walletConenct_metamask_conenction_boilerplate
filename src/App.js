7754c1a0b51bf57dfbac1d6593d99235ea61b84d
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2021-06-24 13:20:24 +0500

    Completed wallet conenct functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Create from "./components/Pages/Create";
 import Layout from "./components/Layout/Layout";
 
+import { Web3ReactProvider } from "@web3-react/core";
+import { getLibrary } from "./utils/walletConnectFunctions";
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Create />
-      </Layout>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Layout>
+          <Create />
+        </Layout>
+      </Web3ReactProvider>
     </div>
   );
 }
